045693a2e3faa139dc0033ecfda26b2d2fe4fafe
brian haltom <brianwh87@gmail.com>
2018-07-16 09:32:16 -0700

    first commit

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -4,8 +4,12 @@ class StorefrontController < ApplicationController
   end
 
   def by_category
+    @products = Product.where(category_id: params[:category_id])
+  	@category = Category.find(params[:category_id])
   end
 
   def by_brand
+  	@products = Product.where(brand: params[:brand])
+    @brand = params[:brand]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+ 
+  get 'cart/add_to_cart'
+  get 'cart/view_order'
+  get 'cart/checkout'
   root 'storefront#all_products'
-  get 'storefront/by_category'
-  get 'storefront/by_brand'
+
+  get 'categories' => 'storefront#by_category'
+  get 'brands' => 'storefront#by_brand'
 
   resources :products
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
